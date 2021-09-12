6fe0e905780d130ca0598e517f6469fe66d19b23
Akash98Sky <a98mondal@gmail.com>
2021-09-12 09:49:46 -0300

    platina: build power stats hal
    fix: Cannot find entry android.hardware.power.stats@1.0::IPowerStats/default in either framework or device manifest.
    Signed-off-by: azrim <mirzaspc@gmail.com>
    Signed-off-by: Ratoriku <a1063021545@gmail.com>
    Change-Id: I0cfe538e048ec19cfbebd1c2069050a0c16340e0

--- device.mk
+++ device.mk
@@ -358,7 +358,9 @@ PRODUCT_PACKAGES += \
 # Power
 PRODUCT_PACKAGES += \
     android.hardware.power-service-qti \
-    vendor.qti.hardware.perf@2.0.vendor
+    vendor.qti.hardware.perf@2.0.vendor \
+    android.hardware.power.stats@1.0-service.mock
+
 
 PRODUCT_COPY_FILES += \
     $(DEVICE_PATH)/configs/power/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json
